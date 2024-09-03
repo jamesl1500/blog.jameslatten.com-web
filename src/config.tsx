b2f0d36835dc03dd6758f85a15f72b4edbbc8043
James Latten <james.latten@phntm.com>
2024-09-03 09:31:52 -0400

    Link fixes

--- src/config.tsx
+++ src/config.tsx
@@ -3,7 +3,7 @@
  */
 
 // Load strapi url from .env
-export const strapiUrl = process.env.REACT_APP_STRAPI_URL || 'http://127.0.0.1:1337';
+export const strapiUrl = process.env.REACT_APP_STRAPI_URL || 'http://api.blog.jameslatten.com/';
 
 // Load strapi url from .env
-export const strapiApiUrl = process.env.REACT_APP_STRAPI_API_URL || 'http://127.0.0.1:1337/api';
+export const strapiApiUrl = process.env.REACT_APP_STRAPI_API_URL || 'http://api.blog.jameslatten.com/api';
